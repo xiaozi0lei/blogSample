b3c28686ff4f60207506bcf0ee16079fd82a76a1
gusun1 <gusun@redhat.com>
2015-01-15 15:55:46 +0800

    scaffold to create blog boneframe and implement comment function

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ActiveRecord::Base
+  # 关联comment模型
+  has_many :comments
+  # 验证，文章必须有title，title的最小长度1
+  validates :title, presence: true,
+                    length: { minimum: 1 }
 end
